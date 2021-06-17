ba3c774dcb7ea4ef472ff7a543ee2a934bc0ed9c
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-06-17 16:20:35 -0400

    Added tiled preview and undo/redo.

--- dotbydot/__main__.py
+++ dotbydot/__main__.py
@@ -13,14 +13,27 @@ DEFAULT_HEIGHT = 240
 X = 0
 Y = 1
 
+COLOR_BLACK = (0, 0, 0)
+COLOR_CYAN = (0, 255, 255)
+COLOR_MAGENTA = (255, 0, 255)
+COLOR_WHITE = (255, 255, 255)
+
+IDX_BLACK = 0
+IDX_CYAN = 1
+IDX_MAGENTA = 2
+IDX_WHITE = 3
+
 class DotByDot( object ):
 
     def __init__(
         self, size_in, size_out, bpp_in, bpp_out,
-        endian_in, endian_out, zoom, interlace_in, interlace_out,
+        endian_in, endian_out, zoom, pzoom, interlace_in, interlace_out,
         filename_in=None, filename_out=None, vertical=False
     ):
+        self.grid_undo = None
+        self.grid_redo = None
         self.zoom = int( zoom )
+        self.preview_zoom = int( pzoom )
         self.size_in = size_in
         # This is handled in main if size_out is None.
         self.size_out = size_out
@@ -158,10 +171,16 @@ class DotByDot( object ):
                 (self.canvas.get_width(), y_grid * self.zoom), 2 )
 
     def set_px( self, px_coords, new_px ):
-        self.grid[int( px_coords[Y] )][int( px_coords[X] )] = new_px
+        if px_coords[X] < self.size_out[X] and \
+        px_coords[Y] < self.size_out[Y]:
+            self.grid[int( px_coords[Y] )][int( px_coords[X] )] = new_px
 
     def toggle_px( self, px_coords ):
         new_px = None
+
+        if px_coords[X] >= self.size_out[X] or \
+        px_coords[Y] >= self.size_out[Y]:
+            return
 
         if 0 == self.grid[int( px_coords[Y] )][int( px_coords[X] )]:
             new_px = 1
@@ -182,6 +201,11 @@ class DotByDot( object ):
     #        self.grid[int( px[Y] )][int( px[X] )] = 1
 
     def erase_px( self, px ):
+
+        if px[X] >= self.size_out[X] or \
+        px[Y] >= self.size_out[Y]:
+            return
+
         if 1 == self.grid[int( px[Y] )][int( px[X] )]:
             self.grid[int( px[Y] )][int( px[X] )] = 0
 
@@ -216,6 +240,30 @@ class DotByDot( object ):
         self.logger.debug( 'bits out: %d (should be %d)', bits_out_total,
             bits )
         assert( bits_out_total == bits )
+
+    def save_undo( self ):
+        self.logger.debug( 'saving state for undo...' )
+        self.grid_undo = []
+        for row in self.grid:
+            self.grid_undo.append( row.copy() )
+
+    def undo_grid( self ):
+        if self.grid_undo:
+            self.logger.debug( 'undoing changes...' )
+            self.grid_redo = self.grid
+            self.grid = self.grid_undo
+            self.grid_undo = None
+        else:
+            self.logger.warning( 'no undo buffer present' )
+        
+    def redo_grid( self ):
+        if self.grid_redo:
+            self.logger.debug( 'redoing changes...' )
+            self.grid_undo = self.grid
+            self.grid = self.grid_redo
+            self.grid_redo = None
+        else:
+            self.logger.warning( 'no redo buffer present' )
 
     def save_grid( self, path ):
 
@@ -261,8 +309,10 @@ class DotByDot( object ):
         return color_draw
 
     def show( self ):
-        self.canvas = pygame.display.set_mode( \
-            (self.size_out[X] * self.zoom, self.size_out[Y] * self.zoom) )
+        canvas_zoom_area_sz = (2 * self.size_out[X]) * self.preview_zoom
+        self.canvas = pygame.display.set_mode(
+            ((self.size_out[X] * self.zoom) + canvas_zoom_area_sz,
+            (self.size_out[Y] * self.zoom)) )
 
         self.running = True
         while self.running:
@@ -277,10 +327,18 @@ class DotByDot( object ):
                         self.running = False
                     elif pygame.K_s == event.key:
                         self.save_grid( self.filename_out )
+                    elif pygame.K_u == event.key:
+                        self.undo_grid()
+                    elif pygame.K_r == event.key:
+                        self.redo_grid()
                     elif pygame.K_q == event.key:
                         self.running = False
                 elif pygame.MOUSEBUTTONDOWN == event.type or \
                 pygame.MOUSEMOTION == event.type:
+
+                    if pygame.MOUSEBUTTONDOWN == event.type:
+                        self.save_undo()
+
                     draw_px = (pygame.mouse.get_pos()[X] / self.zoom, \
                         pygame.mouse.get_pos()[Y] / self.zoom)
                     if (1, 0, 0) == pygame.mouse.get_pressed() and \
@@ -300,7 +358,14 @@ class DotByDot( object ):
                         self.last_coords = (draw_px[X], draw_px[Y])
                         if pygame.MOUSEBUTTONDOWN == event.type:
                             # Only choose a new color if we're clicking.
-                            self.last_px = self.toggle_px( draw_px )
+                            if pygame.key.get_pressed()[pygame.K_1]:
+                                self.last_px = IDX_CYAN
+                            elif pygame.key.get_pressed()[pygame.K_2]:
+                                self.last_px = IDX_MAGENTA
+                            elif pygame.key.get_pressed()[pygame.K_3]:
+                                self.last_px = IDX_WHITE
+                            else:
+                                self.last_px = self.toggle_px( draw_px )
                         self.set_px( draw_px, self.last_px )
                     elif (0, 0, 1) == pygame.mouse.get_pressed():
                         self.logger.debug( 'px erase at %d, %d',
@@ -319,33 +384,75 @@ class DotByDot( object ):
             self.draw_gridlines()
 
             # Draw preview.
-            pzoom = 4
-            p_x = self.canvas.get_width() - (pzoom * self.size_out[X]) - 10
-            p_y = self.canvas.get_height() - (pzoom * self.size_out[Y]) - 10
-            pygame.draw.rect( self.canvas, (255, 255, 255), \
-                pygame.Rect( p_x - pzoom, p_y - pzoom, \
-                    (pzoom * self.size_out[X]) + (2 * pzoom), \
-                    (pzoom * self.size_out[Y]) + (2 * pzoom) ) )
-            pygame.draw.rect( self.canvas, (0, 0, 0), \
-                pygame.Rect( p_x - pzoom, p_y - pzoom, \
-                    (pzoom * self.size_out[X]) + (2 * pzoom), \
-                    (pzoom * self.size_out[Y]) + (2 * pzoom) ), pzoom )
-            for y_grid in range( 0, self.size_out[Y] ):
-                for x_grid in range( 0, self.size_out[X] ):
-                    color_draw = self.color_from_px( self.grid[y_grid][x_grid] )
-                    pygame.draw.rect( self.canvas, color_draw, \
-                        pygame.Rect( \
-                            p_x + (x_grid * pzoom), \
-                            p_y + (y_grid * pzoom), \
-                            pzoom, pzoom ) )
+            p_w = self.preview_zoom * self.size_out[Y]
+            p_h = self.preview_zoom * self.size_out[X]
+            p_x = self.canvas.get_width() - \
+                (1.5 * p_h)
+            p_y = int( (self.size_out[Y] * self.preview_zoom) / 2 )
+
+            self.show_preview( p_x, p_y,
+                0, 0, self.size_out[X], self.size_out[Y] )
+
+            # Tile right.
+            self.show_preview( p_x + p_w, p_y,
+                0, 0, int( self.size_out[X] / 2), self.size_out[Y] )
+
+            # Tile left.
+            self.show_preview( 
+                p_x=p_x - int( p_w / 2 ),
+                p_y=p_y,
+                start_x=int( self.size_out[X] / 2 ),
+                start_y=0,
+                width=int( self.size_out[X] / 2 ),
+                height=self.size_out[Y] )
+
+            # Tile top.
+            self.show_preview(
+                p_x=p_x,
+                p_y=0,
+                start_x=0,
+                start_y=int( self.size_out[Y] / 2 ),
+                width=self.size_out[X],
+                height=int( self.size_out[Y] / 2 ) )
+
+            # Tile bottom.
+            self.show_preview(
+                p_x=p_x,
+                p_y=p_y + (self.size_out[Y] * self.preview_zoom),
+                start_x=0,
+                start_y=0,
+                width=self.size_out[X],
+                height=int( self.size_out[Y] / 2 ) )
 
             pygame.display.flip()
+
+    def show_preview( self, p_x, p_y, start_x, start_y, width, height ):
+        #pygame.draw.rect( self.canvas, (255, 255, 255), \
+        #    pygame.Rect( p_x - self.preview_zoom, p_y - self.preview_zoom, \
+        #        (self.preview_zoom * self.size_out[X]) + (2 * self.preview_zoom), \
+        #        (self.preview_zoom * self.size_out[Y]) + (2 * self.preview_zoom) ) )
+        #pygame.draw.rect( self.canvas, (0, 0, 0), \
+        #    pygame.Rect( p_x - self.preview_zoom, p_y - self.preview_zoom, \
+        #        (self.preview_zoom * self.size_out[X]) + (2 * self.preview_zoom), \
+        #        (self.preview_zoom * self.size_out[Y]) + (2 * self.preview_zoom) ), self.preview_zoom )
+        # Iterate from source coords.
+        for y_grid in range( start_y, start_y + height ):
+            for x_grid in range( start_x, start_x + width ):
+                color_draw = self.color_from_px( self.grid[y_grid][x_grid] )
+                
+                # Draw the individual pixel at this coordinate.
+                pygame.draw.rect( self.canvas, color_draw, \
+                    pygame.Rect( \
+                        p_x + ((x_grid - start_x) * self.preview_zoom), \
+                        p_y + ((y_grid - start_y) * self.preview_zoom), \
+                        self.preview_zoom, self.preview_zoom ) )
 
 if '__main__' == __name__:
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument( '-z', '--zoom', type=float )
+    parser.add_argument( '-z', '--zoom', type=int )
+    parser.add_argument( '-pz', '--pzoom', type=int, default=4 )
     parser.add_argument( '-is', '--insize', nargs="+", type=int )
     parser.add_argument( '-os', '--outsize', nargs="+", type=int )
     parser.add_argument( '-if', '--infile', type=str )
@@ -388,7 +495,7 @@ if '__main__' == __name__:
 
     pygame.init()
     dbd = DotByDot( isize, osize, args.inbpp, args.outbpp, args.inendian,
-        args.outendian, zoom, args.ininterlace, args.outinterlace,
+        args.outendian, zoom, args.pzoom, args.ininterlace, args.outinterlace,
         args.infile, args.outfile, vertical=args.vertical )
     dbd.show()
 
